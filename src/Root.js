4edeb4a4364a7e6504d0de8004ee8de12199e8c9
Prabhjot Singh Kalra <prabhjotkalra3@gmail.com>
2019-11-17 07:12:17 +0530

    creating own middleware

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import reduxPromise from 'redux-promise';
+import async from 'middlewares/async';
 import reducers from 'reducers';
 
 export default ({ children, initialState = {} }) => {
-    const store = createStore(reducers, initialState, compose(applyMiddleware(reduxPromise)))
+    const store = createStore(reducers, initialState, compose(applyMiddleware(async)))
     return  <Provider store={store}>
         {children}
     </Provider>
